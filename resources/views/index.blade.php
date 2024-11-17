a70fb716cd1bf01158f4d8a0e923e53978bf8a74
hudahanif <norhudahanif@gmail.com>
2024-11-17 21:05:49 +0800

    Creating login and register

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
+        <link rel="icon" type="image/x-icon" href="icon.jpg">
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -24,11 +24,11 @@
         <nav class="flex justify-between flex-row p-3  border-b border-gray-300 shadow-sm ">
             <!-- Left Section -->
             <div class="flex items-center">
-                <h5 class="text-primary font-bold text-2xl mr-0 lg:mr-32">ONEUTM</h5> <!-- Increased spacing with mr-8 -->
-                <div class="text-zinc-500 space-x-4"> <!3-- Removed ml-5 -->
-                  <a href="" class="text-primary">Home</a>
-                  <a href="" class="hover:text-primary">Home</a>
-                  <a href="" class="hover:text-primary">Home</a>
+                <h5 class="text-primary font-bold text-2xl mr-0 lg:mr-32">ONEUTM</h5>
+                <div class="text-zinc-500 space-x-4">
+                  <a href="" class="text-primary">Homee</a>
+                  <a href="" class="hover:text-primary">More</a>
+                  <a href="" class="hover:text-primary">About</a>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CatalogController;
 
@@ -7,6 +8,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+<<<<<<< Updated upstream
 // Route to display the catalog items
 Route::get('/marketplace/catalog', [CatalogController::class, 'index'])->name('marketplace.index');
 
@@ -15,3 +17,16 @@ Route::get('/marketplace/product/create', [CatalogController::class, 'create'])-
 
 // Route to store the new product
 Route::post('/marketplace/product', [CatalogController::class, 'store'])->name('marketplace.store');
+=======
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+>>>>>>> Stashed changes
