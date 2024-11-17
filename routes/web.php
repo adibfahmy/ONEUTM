296e57205a4347dc341d493c3bd0877cdf1d78d3
Adib Fahmy <adibroslan@gmail.com>
2024-11-17 21:22:49 +0800

    marketplace route

--- routes/web.php
+++ routes/web.php
@@ -8,16 +8,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< Updated upstream
 // Route to display the catalog items
 Route::get('/marketplace/catalog', [CatalogController::class, 'index'])->name('marketplace.index');
-
 // Route to display the "Add Product" form
 Route::get('/marketplace/product/create', [CatalogController::class, 'create'])->name('marketplace.create');
-
 // Route to store the new product
 Route::post('/marketplace/product', [CatalogController::class, 'store'])->name('marketplace.store');
-=======
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -29,4 +26,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
->>>>>>> Stashed changes
