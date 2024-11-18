a3122702972708fe9fcf9daf18c891b5ed172e5d
002diello <aidielirhan2002@gmail.com>
2024-11-18 08:21:58 +0800

    updating routes web.php

--- routes/web.php
+++ routes/web.php
@@ -25,4 +25,26 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware('auth')->group(function() {
+    Route::get('/parcels', [ParcelController::class, 'index'])->name('parcel.index');
+    Route::get('/parcels/track/', [ParcelController::class, 'index'])->name('parcel.track');
+    Route::get('/parcels/create', [ParcelController::class, 'create'])->name('parcel.create');
+    Route::post('/parcels/store', [ParcelController::class, 'store'])->name('parcel.store');
+    Route::get('/parcels/track/{id}', [ParcelController::class, 'track'])->name('parcel.track');
+    Route::post('/parcels/pickup/{id}', [ParcelController::class, 'pickup'])->name('parcel.pickup');
+    Route::post('/parcels/deliver/{id}', [ParcelController::class, 'deliver'])->name('parcel.deliver');
+    Route::post('/parcels/{parcel}/accept-order', [ParcelController::class, 'acceptOrder'])->name('parcel.acceptOrder');
+    Route::delete('/parcels/{parcel}', [ParcelController::class, 'delete'])->name('parcel.delete');
+    Route::resource('parcels', ParcelController::class);
+    Route::post('/parcels/{parcel}/pickup', [ParcelController::class, 'pickup'])->name('parcel.pickup');
+    Route::post('/parcels/{parcel}/update-status', [ParcelController::class, 'updateStatus'])->name('parcel.updateStatus');
+    Route::get('/parcel-service', [ParcelController::class, 'service'])->name('parcel.service');
+    Route::delete('/parcel/{id}', [ParcelController::class, 'destroy'])->name('parcel.destroy');
+    Route::post('/parcels/{parcel}/accept-order', [ParcelController::class, 'acceptOrder'])->name('parcel.acceptOrder');
+    Route::post('/parcel/{parcel}/cancel', [ParcelController::class, 'cancelOrder'])->name('parcel.cancelOrder');
+    Route::post('/parcel/{parcel}/accept', [ParcelController::class, 'acceptOrder'])->name('parcel.acceptOrder');
+    Route::get('/parcel/{parcel}/track', [ParcelController::class, 'track'])->name('parcel.track');
+    Route::post('/parcel/{parcel}/update-status', [ParcelController::class, 'updateStatus'])->name('parcel.updateStatus');
+});
+
 require __DIR__.'/auth.php';
