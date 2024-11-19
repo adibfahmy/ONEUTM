8aee85a4f764d2781b37641e784c0db62a7ced4d
Adib Fahmy <adibroslan@gmail.com>
2024-11-19 20:10:17 +0800

    include header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,30 +21,7 @@
 
 </head>
 
-<nav class="flex justify-between flex-row p-3  border-b border-gray-300 shadow-sm ">
-    <!-- Left Section -->
-    <div class="flex items-center">
-        <h5 class="text-primary font-bold text-2xl mr-0 lg:mr-32">ONEUTM</h5>
-        <div class="text-zinc-500 space-x-4">
-            <a href="" class="text-primary">Home</a>
-            <a href="" class="hover:text-primary">More</a>
-            <a href="" class="hover:text-primary">About</a>
-        </div>
-    </div>
-
-    <!-- Right Section -->
-    <div class="text-secondary inline-flex items-center">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
-            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-            class="icon icon-tabler icons-tabler-outline icon-tabler-user">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
-            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
-        </svg>
-        <span class="mx-1 text-md">Profile</span>
-    </div>
-</nav>
-
+@include('partials.header')
 
 <main class="flex min-h-96 flex-col justify-center items-center">
     <h1 class="text-4xl text-primary font-bold py-3">ONEUTM</h1>
