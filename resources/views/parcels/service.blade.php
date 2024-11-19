d866437fd8adccce5c29da4f126d252aea08b3d8
Adib Fahmy <adibroslan@gmail.com>
2024-11-19 20:25:45 +0800

    initial update

--- resources/views/parcels/service.blade.php
+++ resources/views/parcels/service.blade.php
@@ -1,5 +1,26 @@
 {{-- resources/views/parcels/service.blade.php --}}
-<x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="icon.jpg">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    @vite('resources/css/app.css')
+
+</head>
+<body>
+
+    @include('partials.header')
     <div class="container mx-auto px-4 py-6">
         <!-- Welcome Message -->
         <div class="bg-white rounded-lg shadow-lg p-6 mb-6">
@@ -70,4 +91,6 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</body>
+</html>
+
